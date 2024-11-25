95b26915e48cb29a70c3bd87faa027bc321648eb
joao <joao@gmail.com>
2024-11-25 20:34:36 -0300

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,7 @@ function App() {
               <div>Aluno: joão victor</div>
             </div>
           </Navbar>  
-
+    <Menu />
     </div>
   );
 }
